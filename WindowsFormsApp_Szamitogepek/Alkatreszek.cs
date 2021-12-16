af7741ff382e3e072564bc8878624ae6aaefed18
kis-eleonora <79101681+kis-eleonora@users.noreply.github.com>
2021-12-16 15:32:04 +0100

    Add project files.

---
+++ WindowsFormsApp_Szamitogepek/Alkatreszek.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp_Szamitogepek
+{
+    class Alkatreszek
+    {
+        readonly string megnevezes;
+        readonly string gyarto;
+        readonly int ar;
+        int darab;
+
+        public Alkatreszek(string megnevezes, string gyarto, int ar, int darab)
+        {
+            this.megnevezes = megnevezes;
+            this.gyarto = gyarto;
+            this.ar = ar;
+            this.Darab = darab;
+        }
+
+        public string Megnevezes => megnevezes;
+
+        public string Gyarto => gyarto;
+
+        public int Ar => ar;
+
+        public int Darab { get => darab; set => darab = value; }
+    }
+}
---
+++ WindowsFormsApp_Szamitogepek/Szamitogepek.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp_Szamitogepek
+{
+    enum Op_rendszerek { Windows, Linux, MacOS };
+    class Szamitogepek
+    {
+        readonly string konfig;
+        Op_rendszerek op_rendszer;
+        string tulajdonos;
+        bool gamer_konfig;
+
+        public string Konfig => konfig;
+
+        public string Tulajdonos { get => tulajdonos; set => tulajdonos = value; }
+        public bool Gamer_konfig { get => gamer_konfig; set => gamer_konfig = value; }
+        internal Op_rendszerek Op_rendszer { get => op_rendszer; set => op_rendszer = value; }
+
+        public Szamitogepek(string konfig, Op_rendszerek op_rendszer, string tulajdonos, bool gamer_konfig)
+        {
+            this.konfig = konfig;
+            this.Op_rendszer = op_rendszer;
+            this.Tulajdonos = tulajdonos;
+            this.Gamer_konfig = gamer_konfig;
+        }
+
+       
+    }
+}
